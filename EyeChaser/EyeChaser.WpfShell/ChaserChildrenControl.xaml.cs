ef2207a5568413d6258cf17457a50fce477c6f4d
Pete Ansell <me@peteansell.com>
2018-08-10 09:38:56 -0700

    Random check-in

---
+++ EyeChaser/EyeChaser.WpfShell/ChaserChildrenControl.xaml.cs
@@ -0,0 +1,70 @@
+﻿using EyeChaser.Model;
+using System;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace EyeChaser.WpfShell
+{
+    /// <summary>
+    /// Interaction logic for ChaserChildrenControl.xaml
+    /// </summary>
+    public partial class ChaserChildrenControl : UserControl
+    {
+        public static readonly DependencyProperty ChildrenProperty = DependencyProperty.Register(nameof(Children), typeof(ReadOnlyObservableCollection<ModelNode>), typeof(ChaserChildrenControl),
+            new PropertyMetadata(null, OnChildrenChanged));
+
+        ReadOnlyObservableCollection<ModelNode> _children;
+
+        public ChaserChildrenControl()
+        {
+            InitializeComponent();
+        }
+
+        public ReadOnlyObservableCollection<ModelNode> Children
+        {
+            get { return (ReadOnlyObservableCollection<ModelNode>)GetValue(ChildrenProperty); }
+            set { SetValue(ChildrenProperty, value); }
+        }
+
+        static void OnChildrenChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var control = (ChaserChildrenControl)d;
+
+            if (control._children != null)
+            {
+                INotifyCollectionChanged collection = control._children;
+                collection.CollectionChanged -= control.OnCollectionChanged;
+            }
+
+            control._children = control.Children;
+
+            if (control._children != null)
+            {
+                INotifyCollectionChanged collection = control._children;
+                collection.CollectionChanged += control.OnCollectionChanged;
+            }
+        }
+
+        void OnCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            TheGrid.Children.Clear();
+            TheGrid.RowDefinitions.Clear();
+
+            var limit = 0.0;
+            var rowCount = 0;
+            foreach (var child in _children)
+            {
+                if (limit < child.VisibleRange.LowerBound)
+                {
+                    TheGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(child.VisibleRange.LowerBound - limit, GridUnitType.Star) });
+                    rowCount++;
+                }
+
+                TheGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(child.BoundSize, GridUnitType.Star) });
+
+            }
+        }
+    }
+}
---
+++ EyeChaser/EyeChaser.WpfShell/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using EyeChaser.Model;
+using EyeChaser.Queries;
+using EyeChaser.WpfShell.Properties;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Packaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace EyeChaser.WpfShell
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Loaded += OnLoaded;
+        }
+
+        async void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            var xml = Properties.Resources.MockData;
+            var stream = new StringReader(xml);
+            //var installationFolder = Package.Current.InstalledLocation;
+            //var file = await installationFolder.GetFileAsync(@"Assets\MockData.xml");
+            //var stream = await file.OpenStreamForReadAsync();
+            var settings = new XmlReaderSettings { Async = true };
+            var reader = XmlReader.Create(stream, settings);
+
+            var engine = await XmlChaserQueryEngine.CreateAsync(reader);
+            var root = new RootModel(engine.Root);
+            await root.UpdateAsync(0.01);
+        }
+    }
+}
