1c629fd5d363ddb27ff47af54871e1ba12f09e14
Alan Lawrence (MSRC) <allawr@microsoft.com>
2018-08-14 19:05:54 +0100

    Add DynamicSwiftKeyModel (not used yet)

---
+++ EyeChaser/DynamicSwiftKeyModel/Context.cs
@@ -0,0 +1,70 @@
+// TouchType SwiftKey SDK
+// Copyright TouchType 2012
+
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Collections.Generic;
+using TouchType.TextPrediction;
+using System.Diagnostics;
+
+namespace EyeChaser.DynamicSwiftKeyModel
+{
+    class Context : IDisposable
+    {
+        private readonly Session session;
+
+        private CapitalizationHint capitalization = CapitalizationHint.Default;
+        private ModelSelector selector = ModelSelectors.AllModels;
+
+        //TODO insert private license key
+        private static string License = ;
+
+        public Context()
+        {
+            RegisterLogger();
+
+            session = SwiftKeySDK.CreateSession(License);
+
+            session.Trainer.SetParameterLearning(false); // prevents prefixes from being over-penalized
+
+            Debug.WriteLine("SwiftKey SDK {0}...", SwiftKeySDK.Version);
+        }
+
+        public void Dispose()
+        {
+            session.Dispose();
+        }
+
+        public Session Session { get { return session; } }
+        public CapitalizationHint Capitalization
+        {
+            get { return capitalization; }
+            set { capitalization = value; }
+        }
+        public ModelSelector Selector
+        {
+            get { return selector; }
+            set { selector = value; }
+        }
+
+        private static void RegisterLogger()
+        {
+            SwiftKeySDK.Log += new LogHandler(delegate(LogLevel level, string message) 
+            {
+                Debug.WriteLine("[{0}]: {1}", level, message);
+            });
+        }
+
+        public void Load(string arguments)
+        {
+            string directory = arguments.Split(' ')[0];
+
+            var description = ModelSetDescription.FromFile(directory);
+
+            Session.Load(description);
+
+            Debug.WriteLine("Successfully loaded \"{0}\"", directory);
+        }
+    }
+}
---
+++ EyeChaser/DynamicSwiftKeyModel/SwiftKeyNode.cs
@@ -0,0 +1,95 @@
+﻿using EyeChaser.Api;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TouchType.TextPrediction;
+
+namespace EyeChaser.DynamicSwiftKeyModel
+{
+    public class SwiftKeyNode : IChaserNode
+    {
+        public static SwiftKeyNode CreateRoot()
+        {
+            Context cs = new Context();
+            cs.Load("C:\\Users\\allawr\\Downloads\\en_gb");
+            //We'll never actually dispose that Context, but it wants to live as long as *any* node
+            return new SwiftKeyNode(cs, new Sequence());
+        }
+
+        // Each time we look for predictions, include everything above this, and one word for each keyboard character
+        protected const double WordProbThreshold = 0.1;
+        protected const int NResults = (int)(1.0 / WordProbThreshold);
+
+        private static readonly ResultsFilter DefaultFilter =
+            new ResultsFilter(NResults, CapitalizationHint.Default, VerbatimMode.Enabled);
+
+        private Context _context;
+        private Sequence _seq;
+
+        SwiftKeyNode(Context context, Sequence seq)
+        {
+            _context = context;
+            _seq = seq;
+        }
+
+        public string Caption { get; private set; }
+
+        public double Probability { get; private set; }
+
+        private static readonly IReadOnlyList<SwiftKeyNode> empty = new SwiftKeyNode[0];
+        private IReadOnlyList<SwiftKeyNode> _children;
+
+        public IEnumerator<SwiftKeyNode> GetEnumerator()
+        {
+            return (_children ?? null).GetEnumerator();
+        }
+
+        public Task<IEnumerable<IChaserNode>> GetChildrenAsync(double precision)
+        {
+            if (_children != null) return Task.FromResult<IEnumerable<IChaserNode>>(_children);
+            return Task.Run<IEnumerable<IChaserNode>>(() =>
+            {
+                Prediction[] predictions = _context.Session.Predictor.GetPredictions(
+                    _seq, new TouchHistory(), DefaultFilter.With(_context.Capitalization));
+                
+                double totProb = predictions.Select(p => p.Probability).Sum();
+                //Assume that's 100%, which is clearly bogus
+                var seenEachChar = new Boolean['z' - 'a'];
+
+                //int distinctCharsSeen = 0;
+                var keptPredictions = predictions
+                    .TakeWhile(p => {
+                        int idx = p.FullPrediction.ToLower().First() - 'a';
+                        bool forceInclude = false;// distinctCharsSeen < seenEachChar.Length;
+                        if (idx >= 0 && idx < seenEachChar.Length)
+                        {
+                            if (!seenEachChar[idx])
+                            {
+                                seenEachChar[idx] = true;
+                                //distinctCharsSeen++;
+                                forceInclude = true;
+                            }
+                        }
+                        return (p.Probability / totProb > WordProbThreshold) || forceInclude;
+                    }).ToArray();
+                totProb = keptPredictions.Select(p => p.Probability).Sum();
+                _children = keptPredictions.Select(p =>
+                    {
+                        Sequence newSeq = _seq.DropLast(0); // Clone.
+                        newSeq.AddLast(p.FullPrediction);
+                        return new SwiftKeyNode(_context, _seq)
+                        {
+                            Caption = p.FullPrediction,
+                            Probability = p.Probability / totProb
+                        };
+                    }).ToArray();
+                return _children;
+            });
+        }
+    }
+}
